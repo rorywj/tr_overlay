ca3c5d1720c1187b525209d068962e4193920556
rorywj <62103366+rorywj@users.noreply.github.com>
2022-01-25 00:10:32 +0000

    Update tr_darkmode.js

--- tr_darkmode.js
+++ tr_darkmode.js
@@ -31,13 +31,13 @@ function set_darkmode(obj) {
           off.setFullYear(now_year, now_month, now_date); //Set 'off' date to today
           var off_time = off.getTime();
           if (time_now > off_time && time_now < on_time) { //If time now is between off time and on time
-              //Activate Light mode
-              lightswitch();
-              on_timecheck(on_time, off_time);
+              	//Activate Light mode
+              	lightswitch();
+              	on_timecheck(on_time, off_time);
           } else { //If time now is after on time or before off time
-              //Activate Dark mode
-              darkswitch();
-			  off_timecheck(on_time, off_time);	  
+              	//Activate Dark mode
+              	darkswitch();
+		off_timecheck(on_time, off_time);	  
           }
       }
       if(obj.dm_mode == "On") {	
@@ -57,7 +57,7 @@ function on_timecheck(on_time, off_time) {
   	var switch_on_in = (on_time - time_n2); //Time left until dark mode should turn on
   	console.log("Switch dark mode on in: " + switch_on_in);
 	var timeout_n2 = Math.min(Math.max(switch_on_in, 1000), 3600000); //Make sure timeout is at least 1000ms
-    setTimeout(readKV, timeout_n2); //Read dark mode kv again after timeout
+    setTimeout(get_dm_kv, timeout_n2); //Read dark mode kv again after timeout
 }
 
 function off_timecheck(on_time, off_time) {
@@ -68,6 +68,6 @@ function off_timecheck(on_time, off_time) {
       	var switch_off_in = (off_time - time_n3); //Time left until dark mode turns off
       	console.log("Switch dark mode off in: " + switch_off_in);
       	var timeout_n3 = Math.min(Math.max(switch_off_in, 1000), 3600000); //Make sure timeout is at least 1000ms
-      	setTimeout(readKV, timeout_n3); //Read dark mode kv again after timeout	
+      	setTimeout(get_dm_kv, timeout_n3); //Read dark mode kv again after timeout	
     } 	
 }
